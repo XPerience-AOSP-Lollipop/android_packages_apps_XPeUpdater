LineageOS/android_packages_apps_CMUpdater

Move properties parsing logic inside Utils
Also, try to use dedicated props where possible.

Change-Id: I499c421f077330e9e2ddda4f3c0556c87b66e060

src/com/cyanogenmod/updater/UpdatesActivity.java
@@ -10,6 +10,7 @@ package com.cyanogenmod.updater;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
@@ -31,7 +32,6 @@ public class UpdatesActivity extends AppCompatActivity {
 
     private TextView mHeaderInfo;
     private UpdatesSettings mSettingsFragment;
-    private String[] mInstalled;
 
     @Override
     protected void onCreate(Bundle savedInstance) {
@@ -44,8 +44,8 @@ public class UpdatesActivity extends AppCompatActivity {
 
         mSettingsFragment = new UpdatesSettings();
 
-        mInstalled = Utils.getInstalledVersion().split("-");
-        headerCm.setText(String.format(getString(R.string.header_os), mInstalled[0]));
+        final String version = Utils.getInstalledVersionName();
+        headerCm.setText(String.format(getString(R.string.header_os), version));
 
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.content_frame, mSettingsFragment).commit();
@@ -113,18 +113,9 @@ public class UpdatesActivity extends AppCompatActivity {
     }
 
     private void updateHeader() {
-        String api = String.valueOf(Utils.getInstalledApiLevel());
-        switch (api) {
-            case "25":
-                api = "7.1.1";
-                break;
-            default:
-                api = "API " + api;
-        }
-
         mHeaderInfo.setText(String.format(getString(R.string.header_summary),
-                Utils.getInstalledBuildDateLocalized(this, mInstalled[1]),
-                mInstalled[2], api, getLastCheck()));
+                Utils.getInstalledBuildDateLocalized(this, Utils.getInstalledBuildDate()),
+                Utils.getInstalledBuildType(), Build.VERSION.RELEASE, getLastCheck()));
     }
 
     private String getLastCheck() {

src/com/cyanogenmod/updater/utils/Utils.java
@@ -19,6 +19,7 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Build;
 import android.os.SystemProperties;
 import android.preference.PreferenceManager;
 
@@ -28,8 +29,9 @@ import com.cyanogenmod.updater.service.UpdateCheckService;
 
 import java.io.File;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class Utils {
     private Utils() {
@@ -55,6 +57,10 @@ public class Utils {
         return SystemProperties.get("ro.cm.version");
     }
 
+    public static String getInstalledVersionName() {
+        return getInstalledVersion().split("-")[0];
+    }
+
     public static int getInstalledApiLevel() {
         return SystemProperties.getInt("ro.build.version.sdk", 0);
     }
@@ -63,39 +69,25 @@ public class Utils {
         return SystemProperties.getLong("ro.build.date.utc", 0);
     }
 
-    /**
-     * Extract date from build YYYYMMDD date
-     *
-     * @param context for getting localized string
-     *
-     * @return MMMM dd, yyyy formatted date (or localized translation)
-     */
-    public static String getInstalledBuildDateLocalized(Context context, String buildDate) {
-        if (buildDate.length() < 8) {
-            return "";
+    public static String getInstalledBuildType() {
+        return SystemProperties.get("ro.cm.releasetype", Constants.CM_RELEASETYPE_UNOFFICIAL);
+    }
+
+    public static String getInstalledBuildDateLocalized(Context context, long unixTimestamp) {
+        DateFormat f = DateFormat.getDateInstance(DateFormat.LONG, getCurrentLocale(context));
+        Date date = new Date(unixTimestamp * 1000);
+        return f.format(date);
+    }
+
+    public static String getAndroidVersion(String versionName) {
+        switch (versionName) {
+            case "13.0":
+                return "6.0";
+            case "14.1":
+                return "7.1";
+            default:
+                return "???";
         }
-
-        Calendar cal = Calendar.getInstance();
-        cal.set(Calendar.YEAR, Integer.parseInt(buildDate.substring(0, 4)));
-        cal.set(Calendar.MONTH, Integer.parseInt(buildDate.substring(4, 6)) - 1);
-        cal.set(Calendar.DAY_OF_MONTH, Integer.parseInt(buildDate.substring(6, 8)));
-
-        String date = new SimpleDateFormat(context.getString(R.string.date_formatting),
-                        context.getResources().getConfiguration().locale).format(cal.getTime());
-
-        int position = 0;
-        boolean mWorking = true;
-        while (mWorking) {
-            if (!Character.isDigit(date.charAt(position))) {
-                mWorking = false;
-            } else {
-                position++;
-            }
-        }
-
-        return date.substring(0, position) +
-                String.valueOf(date.charAt(position)).toUpperCase() +
-                date.substring(position + 1, date.length());
     }
 
     public static String getUserAgentString(Context context) {
@@ -170,4 +162,27 @@ public class Utils {
         }
         return updateType;
     }
+
+    public static String buildTypeToString(int type) {
+        switch (type) {
+            case Constants.UPDATE_TYPE_SNAPSHOT:
+                return Constants.CM_RELEASETYPE_SNAPSHOT;
+            case Constants.UPDATE_TYPE_NIGHTLY:
+                return Constants.CM_RELEASETYPE_NIGHTLY;
+            case Constants.UPDATE_TYPE_EXPERIMENTAL:
+                return Constants.CM_RELEASETYPE_EXPERIMENTAL;
+            default:
+                return Constants.CM_RELEASETYPE_UNOFFICIAL;
+        }
+    }
+
+    public static Locale getCurrentLocale(Context context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            return context.getResources().getConfiguration().getLocales()
+                    .getFirstMatch(context.getResources().getAssets().getLocales());
+        } else {
+            return context.getResources().getConfiguration().locale;
+        }
+    }
+
 }

XPerience-AOSP-Lollipop/android_packages_apps_XPeUpdater

Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

src/com/cyanogenmod/updater/UpdatesActivity.java
@@ -10,6 +10,7 @@ package com.cyanogenmod.updater;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
@@ -31,7 +32,6 @@ public class UpdatesActivity extends AppCompatActivity {
 
     private TextView mHeaderInfo;
     private UpdatesSettings mSettingsFragment;
-    private String[] mInstalled;
 
     @Override
     protected void onCreate(Bundle savedInstance) {
@@ -44,8 +44,8 @@ public class UpdatesActivity extends AppCompatActivity {
 
         mSettingsFragment = new UpdatesSettings();
 
-        mInstalled = Utils.getInstalledVersion().split("-");
-        mHeaderCm.setText(String.format(getString(R.string.header_os), mInstalled[0]));
+        final String version = Utils.getInstalledVersionName();
+        headerCm.setText(String.format(getString(R.string.header_os), version));
 
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.content_frame, mSettingsFragment).commit();
@@ -113,18 +113,9 @@ public class UpdatesActivity extends AppCompatActivity {
     }
 
     private void updateHeader() {
-        String mApi = String.valueOf(Utils.getInstalledApiLevel());
-        switch (mApi) {
-            case "25":
-                mApi = "7.1.1";
-                break;
-            default:
-                mApi = "API " + mApi;
-        }
-
         mHeaderInfo.setText(String.format(getString(R.string.header_summary),
-                Utils.getInstalledBuildDateLocalized(this, mInstalled[1]),
-                mInstalled[2], mApi, getLastCheck()));
+                Utils.getDateLocalized(this, Utils.getInstalledBuildDate()),
+                Utils.getInstalledBuildType(), Build.VERSION.RELEASE, getLastCheck()));
     }
 
     private String getLastCheck() {

src/com/cyanogenmod/updater/utils/Utils.java
@@ -19,6 +19,7 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.Build;
 import android.os.SystemProperties;
 import android.preference.PreferenceManager;
 
@@ -28,8 +29,10 @@ import com.cyanogenmod.updater.service.UpdateCheckService;
 
 import java.io.File;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 public class Utils {
     private Utils() {
@@ -55,6 +58,10 @@ public class Utils {
         return SystemProperties.get("ro.xpe.version");
     }
 
+    public static String getInstalledVersionName() {
+        return getInstalledVersion().split("-")[0];
+    }
+
     public static int getInstalledApiLevel() {
         return SystemProperties.getInt("ro.build.version.sdk", 0);
     }
@@ -63,39 +70,27 @@ public class Utils {
         return SystemProperties.getLong("ro.build.date.utc", 0);
     }
 
-    /**
-     * Extract date from build YYYYMMDD date
-     *
-     * @param mContext for getting localized string
-     *
-     * @return MMMM dd, yyyy formatted date (or localized translation)
-     */
-    public static String getInstalledBuildDateLocalized(Context mContext, String mBuildDate) {
-        if (mBuildDate.length() < 8) {
-            return "";
+    public static String getInstalledBuildType() {
+        return SystemProperties.get("ro.xpe.releasetype", Constants.CM_RELEASETYPE_UNOFFICIAL);
+    }
+
+    public static String getDateLocalized(Context context, long unixTimestamp) {
+        DateFormat f = DateFormat.getDateInstance(DateFormat.LONG, getCurrentLocale(context));
+        f.setTimeZone(TimeZone.getTimeZone("UTC"));
+        Date date = new Date(unixTimestamp * 1000);
+        return f.format(date);
+    }
+
+    public static String getAndroidVersion(String versionName) {
+        switch (versionName) {
+            case "10.0":
+                return "6.0";
+            case "11.1":
+                return "7.1";
+            default:
+                return "???";
         }
 
-        Calendar mCal = Calendar.getInstance();
-        mCal.set(Calendar.YEAR, Integer.parseInt(mBuildDate.substring(0, 4)));
-        mCal.set(Calendar.MONTH, Integer.parseInt(mBuildDate.substring(4, 6)) - 1);
-        mCal.set(Calendar.DAY_OF_MONTH, Integer.parseInt(mBuildDate.substring(6, 8)));
-
-        String mDate = new SimpleDateFormat(mContext.getString(R.string.date_formatting),
-                        mContext.getResources().getConfiguration().locale).format(mCal.getTime());
-
-        int mPosition = 0;
-        boolean mWorking = true;
-        while (mWorking) {
-            if (!Character.isDigit(mDate.charAt(mPosition))) {
-                mWorking = false;
-            } else {
-                mPosition++;
-            }
-        }
-
-        return mDate.substring(0, mPosition) +
-                String.valueOf(mDate.charAt(mPosition)).toUpperCase() +
-                mDate.substring(mPosition + 1, mDate.length());
     }
 
     public static String getUserAgentString(Context context) {
@@ -170,4 +165,27 @@ public class Utils {
         }
         return updateType;
     }
+
+    public static String buildTypeToString(int type) {
+        switch (type) {
+            case Constants.UPDATE_TYPE_SNAPSHOT:
+                return Constants.CM_RELEASETYPE_SNAPSHOT;
+            case Constants.UPDATE_TYPE_NIGHTLY:
+                return Constants.CM_RELEASETYPE_NIGHTLY;
+            case Constants.UPDATE_TYPE_EXPERIMENTAL:
+                return Constants.CM_RELEASETYPE_EXPERIMENTAL;
+            default:
+                return Constants.CM_RELEASETYPE_UNOFFICIAL;
+        }
+    }
+
+    public static Locale getCurrentLocale(Context context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            return context.getResources().getConfiguration().getLocales()
+                    .getFirstMatch(context.getResources().getAssets().getLocales());
+        } else {
+            return context.getResources().getConfiguration().locale;
+        }
+    }
+
 }
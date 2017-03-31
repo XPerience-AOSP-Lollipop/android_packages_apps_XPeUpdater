LineageOS/android_packages_apps_CMUpdater

Log package verification exception
The verification can fail because of different reasons. Add a
Log line so that we can see the reason of the failure.
While at it, add some other log lines.

Change-Id: Ie4eae136408097852ce0233c262e1bce8d8b708f

src/com/cyanogenmod/updater/service/DownloadCompleteIntentService.java
@@ -15,6 +15,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.ParcelFileDescriptor;
+import android.util.Log;
 
 import com.cyanogenmod.updater.R;
 import com.cyanogenmod.updater.UpdatesActivity;
@@ -31,6 +32,9 @@ import java.io.IOException;
 import java.nio.channels.FileChannel;
 
 public class DownloadCompleteIntentService extends IntentService {
+
+    private static final String TAG = "DownloadComplete";
+
     private DownloadManager mDm;
 
     @Override
@@ -46,6 +50,7 @@ public class DownloadCompleteIntentService extends IntentService {
     @Override
     protected void onHandleIntent(Intent intent) {
         if (!intent.hasExtra(Constants.DOWNLOAD_ID) || !intent.hasExtra(Constants.DOWNLOAD_NAME)) {
+            Log.d(TAG, "Missing intent extra data");
             return;
         }
 
@@ -72,6 +77,7 @@ public class DownloadCompleteIntentService extends IntentService {
                 FileChannel outChannel = outStream.getChannel();
                 inChannel.transferTo(0, inChannel.size(), outChannel);
             } catch (IOException e) {
+                Log.d(TAG, "Copy of download failed", e);
                 displayErrorResult(updateIntent, R.string.unable_to_download_file);
                 return;
             } finally {
@@ -82,6 +88,7 @@ public class DownloadCompleteIntentService extends IntentService {
             try {
                 android.os.RecoverySystem.verifyPackage(destFile, null, null);
             } catch (Exception e) {
+                Log.d(TAG, "Verification failed", e);
                 if (destFile.exists()) {
                     destFile.delete();
                 }
@@ -95,6 +102,7 @@ public class DownloadCompleteIntentService extends IntentService {
                     destPath);
             displaySuccessResult(updateIntent, destFile);
         } else if (status == DownloadManager.STATUS_FAILED) {
+            Log.d(TAG, "Download failed");
             // The download failed, reset
             mDm.remove(id);
             displayErrorResult(updateIntent, R.string.unable_to_download_file);

XPerience-AOSP-Lollipop/android_packages_apps_XPeUpdater

src/com/cyanogenmod/updater/service/DownloadCompleteIntentService.java
@@ -15,6 +15,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.ParcelFileDescriptor;
+import android.util.Log;
 
 import com.cyanogenmod.updater.R;
 import com.cyanogenmod.updater.UpdatesActivity;
@@ -31,6 +32,9 @@ import java.io.IOException;
 import java.nio.channels.FileChannel;
 
 public class DownloadCompleteIntentService extends IntentService {
+
+    private static final String TAG = "DownloadComplete";
+
     private DownloadManager mDm;
 
     @Override
@@ -46,6 +50,7 @@ public class DownloadCompleteIntentService extends IntentService {
     @Override
     protected void onHandleIntent(Intent intent) {
         if (!intent.hasExtra(Constants.DOWNLOAD_ID) || !intent.hasExtra(Constants.DOWNLOAD_NAME)) {
+            Log.e(TAG, "Missing intent extra data");
             return;
         }
 
@@ -72,6 +77,7 @@ public class DownloadCompleteIntentService extends IntentService {
                 FileChannel outChannel = outStream.getChannel();
                 inChannel.transferTo(0, inChannel.size(), outChannel);
             } catch (IOException e) {
+                Log.e(TAG, "Copy of download failed", e);
                 displayErrorResult(updateIntent, R.string.unable_to_download_file);
                 return;
             } finally {
@@ -82,6 +88,7 @@ public class DownloadCompleteIntentService extends IntentService {
             try {
                 android.os.RecoverySystem.verifyPackage(destFile, null, null);
             } catch (Exception e) {
+                Log.e(TAG, "Verification failed", e);
                 if (destFile.exists()) {
                     destFile.delete();
                 }
@@ -95,6 +102,7 @@ public class DownloadCompleteIntentService extends IntentService {
                     destPath);
             displaySuccessResult(updateIntent, destFile);
         } else if (status == DownloadManager.STATUS_FAILED) {
+            Log.e(TAG, "Download failed");
             // The download failed, reset
             mDm.remove(id);
             displayErrorResult(updateIntent, R.string.unable_to_download_file);